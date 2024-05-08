be1b8187bb0d1f9c538f2670383138eaa5d38a45
Faith M. Roberts <faithroberts708@gmail.com>
2024-05-08 11:48:13 +0100

    adding logic

---
+++ src/crime-evidence/CrimeRecord.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721Pausable.sol";
+import "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import "./ERC2771Recipient.sol";
+
+contract CrimeRecord is ERC721, ERC721URIStorage, Ownable, ERC721Pausable, ERC2771Recipient {
+    uint256 private _nextTokenId;
+    address trustedForwarder;
+
+    constructor(address initialOwner, address _trustedForwarder)
+        ERC721("CrimeRecords", "CRD")
+        Ownable(initialOwner)
+    {trustedForwarder = _trustedForwarder;}
+
+    function coverCrime(string memory uri) public {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(_msgSender(), tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    function setTrustedForwarder(address _forwarder) external onlyOwner() {
+        _setTrustedForwarder(_forwarder);
+    }
+
+
+     function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+  function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721, ERC721Pausable)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function _msgSender() internal override(Context, ERC2771Recipient) virtual view returns (address ret) {
+       ret = super._msgSender();
+    }
+
+    function _msgData() internal override(Context, ERC2771Recipient) virtual view returns (bytes calldata ret) {
+        ret = super._msgData();
+    }
+
+    
+}
--- test/crime.t.sol
+++ test/crime.t.sol
@@ -2,7 +2,31 @@
 pragma solidity ^0.8.20;
 
 import {Test, console} from "../lib/forge-std/src/Test.sol";
+import {CrimeRecord} from "../src/crime-evidence/CrimeRecord.sol";
 
-contract CounterTest is Test {
+contract CrimeTest is Test {
+    CrimeRecord record;
 
+    address owner = mkaddr('owner');
+
+    function setUp() public {
+        vm.startPrank(owner);
+        record = new CrimeRecord(owner, owner); 
+        vm.stopPrank();
+    }
+
+    function testCoverCrime() public {
+        vm.startPrank(owner);
+        record.coverCrime('uri');
+        assert(record.balanceOf(owner) == 1);
+        vm.stopPrank();
+    }
+
+      function mkaddr(string memory name) public returns (address) {
+        address addr = address(
+            uint160(uint256(keccak256(abi.encodePacked(name))))
+        );
+        vm.label(addr, name);
+        return addr;
+    }
 }
